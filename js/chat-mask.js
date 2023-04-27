44f737ea2e6ea2288a865bc585007a9080e2c515
Maria Rios Navarro <propscode254@gmail.com>
2023-04-27 20:13:23 +0200

    Add more JS

--- js/chat-mask.js
+++ js/chat-mask.js
@@ -1,5 +1,21 @@
 // ------- A------appearance of the saved name in the form
 
+// ------ User-----
+
+const chatNamePhoneInput = document.querySelector(
+  '[data-js="chatname-phone-input"]'
+);
+
+const chatNamePhoneValue = document.querySelector(
+  '[data-js="chatname-phone-value"]'
+);
+
+chatNamePhoneInput.addEventListener("input", () => {
+  chatNamePhoneValue.textContent = chatNamePhoneInput.value;
+});
+
+//  ----Chat Name-------
+
 const chatNameOutInput = document.querySelector(
   '[data-js="chatname-out-input"]'
 );
