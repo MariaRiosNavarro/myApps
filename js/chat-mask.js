b294d8d43d5bcb8207daee858b05c0bb24dca58e
Maria Rios Navarro <propscode254@gmail.com>
2023-04-27 20:33:29 +0200

    Add more JS

--- js/chat-mask.js
+++ js/chat-mask.js
@@ -27,4 +27,33 @@ chatNameOutInput.addEventListener("input", () => {
   chatNameOutValue.textContent = chatNameOutInput.value;
 });
 
+//-------Save/Delete------------
+
+const saveButton = document.querySelector('[data-js="save-button"]');
+const clearButton = document.querySelector('[data-js="clear-button"]');
+
+// Save the values of the input fields by clicking on the save button
+saveButton.addEventListener("click", () => {
+  const formValues = {
+    chatNameOut: chatNameOutInput.value,
+  };
+  localStorage.setItem("formValues", JSON.stringify(formValues));
+  alert("Formular gespeichert");
+});
+
+// Delete saved values and reload the page by clicking on the delete button
+clearButton.addEventListener("click", () => {
+  localStorage.removeItem("formValues");
+  location.reload();
+});
+
+// Get saved values on page load
+const savedValues = JSON.parse(localStorage.getItem("formValues")) || {};
+chatNameOutInput.value = savedValues.chatNameOut || "";
+
+// Update the value of the span by entering the value
+chatNameOutInput.addEventListener("input", () => {
+  chatNameOutValue.textContent = chatNameOutInput.value;
+});
+
 // ----- end A
